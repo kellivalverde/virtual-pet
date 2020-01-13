dfb2db5358070948428d0082725ad684ff6089e5
kellivalverde <paughka@gmail.comls>
2020-01-12 20:33:41 -0500

    improved loop

--- src/main/java/virtualpet/VirtualPetApp.java
+++ src/main/java/virtualpet/VirtualPetApp.java
@@ -17,14 +17,14 @@ public class VirtualPetApp {
 		while (true) {
 
 			System.out.println("Here's how I feel...");
-																		// could I clean this up with an array?
+			// could I clean this up with an array?
 			System.out.println("Hunger: " + myPet.getHunger());
 			System.out.println("Thirst: " + myPet.getThirst());
 			System.out.println("Poop level: " + myPet.getPoop());
 			System.out.println("Boredom: " + myPet.getBoredom());
 
 			System.out.println("\nHint: Keep my stats down to keep me a happy pet.");
-						
+
 			System.out.println("\nSo, what would you like to do? \n");
 			System.out.println("Press 1 to feed me");
 			System.out.println("Press 2 to give me water");
@@ -38,39 +38,36 @@ public class VirtualPetApp {
 			if (menuOption.contentEquals("1")) {
 				System.out.println("Yum yum! I love food.");
 				myPet.feed();
-				//myPet.getHunger --;
-								
-			} else if(menuOption.contentEquals("2")) {
+
+			} else if (menuOption.contentEquals("2")) {
 				System.out.println("Glug glug! Thanks for the water!");
 				myPet.giveWater();
-			}	
-			else if(menuOption.contentEquals("3")) {
+			} else if (menuOption.contentEquals("3")) {
 				System.out.println("Thanks! It was getting stinky in here.");
 				myPet.cleanPoop();
-			}
-			 else if(menuOption.contentEquals("4")) {
+					
+			} else if (menuOption.contentEquals("4")) {
 				System.out.println("Yay! Let's play!");
 				myPet.play();
-								
- 			} else if(menuOption.contentEquals("5")) {
+
+			} else if (menuOption.contentEquals("5")) {
 				System.out.println("...Ok");
-										
- 			} else if(menuOption.contentEquals("6")) {
- 				System.out.println("Aw man... Ok, I guess I'll see you later.");
+
+			} else if (menuOption.contentEquals("6")) {
+				System.out.println("Aw man... Ok, I guess I'll see you later.");
 				break;
 			} else {
 				System.out.println("What?! Say that again.");
 			}
 
 			myPet.tick();
-			
+
 			System.out.println("Press enter to see how I feel now.");
 			input.nextLine();
-			
-			
+
 		} // loop end
 
-	input.close();
-}
+		input.close();
+	}
 
 }
